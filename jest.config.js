45e563a6415006d8fe76512003cbb472cb40f4ab
VictorNaB <92879126+VictorNaB@users.noreply.github.com>
2025-06-12 11:56:57 -0500

    Add files via upload

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    roots: ['<rootDir>/src'],
+    testMatch: ['**/__tests__/**/*.test.ts'],
+    transform: {
+        '^.+\\.tsx?$': ['ts-jest', {
+            tsconfig: 'tsconfig.json'
+        }]
+    },
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/src/$1'
+    },
+    setupFilesAfterEnv: ['<rootDir>/src/config/test/setup.ts'],
+    testTimeout: 10000,
+    verbose: true
+}; 
---
+++ jest.setup.js
@@ -0,0 +1,26 @@
+// Guardar las funciones originales de console
+const originalConsole = {
+    log: console.log,
+    error: console.error,
+    warn: console.warn,
+    info: console.info,
+    debug: console.debug
+};
+
+// Silenciar todos los console logs durante las pruebas
+global.beforeAll(() => {
+    console.log = jest.fn();
+    console.error = jest.fn();
+    console.warn = jest.fn();
+    console.info = jest.fn();
+    console.debug = jest.fn();
+});
+
+// Restaurar las funciones originales después de las pruebas
+global.afterAll(() => {
+    console.log = originalConsole.log;
+    console.error = originalConsole.error;
+    console.warn = originalConsole.warn;
+    console.info = originalConsole.info;
+    console.debug = originalConsole.debug;
+}); 
