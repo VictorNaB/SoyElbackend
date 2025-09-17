6120f1fcccaaa774389763069fc2f346f38d0106
VictorNaB <92879126+VictorNaB@users.noreply.github.com>
2025-09-17 09:18:35 -0500

    Update Sql_Accesos.ts

--- src/app/accesos/repository/Sql_Accesos.ts
+++ src/app/accesos/repository/Sql_Accesos.ts
@@ -1,16 +1,14 @@
-<<<<<<< HEAD
+
 export const sql_Accesos={
     
-=======
+
 export const sql_Accesos = {
->>>>>>> 62f9d91 (Cambios realizados)
     GETALL: `SELECT * FROM accesos`,
     getById: `SELECT * FROM accesos WHERE cod_usuario = $1`,
     ADD: `INSERT INTO accesos (cod_usuario, correo_acceso, clave_acceso, uuid_acceso) VALUES ($1, $2, $3, $4) returning cod_usuario`,
     UPDATE: `UPDATE accesos SET correo_acceso = $2, clave_acceso = $3, uuid_acceso = $4 WHERE cod_usuario = $1`,
     DELETE: `DELETE FROM accesos WHERE cod_usuario = $1`,
     getByCorreo: `SELECT * FROM accesos WHERE correo_acceso = $1`,
-<<<<<<< HEAD
     getByUuid: `SELECT * FROM accesos WHERE uuid_acceso = $1`, // Agregado para obtener por UUID
     getByUserId: `SELECT cod_usuario FROM usuarios WHERE cod_usuario = $1`,
     DevolverUsuario: "Select u.cod_usuario as codUsuario, u.nombres_usuario as nombreUsuario,",
@@ -22,7 +20,7 @@ export const sql_Accesos = {
         clave_acceso as claveAcceso, \
         uuid_acceso as uuidAcceso"
 }
-=======
+
     getByUuid: `SELECT * FROM accesos WHERE uuid_acceso = $1`,
     getByUserId: `SELECT cod_usuario FROM usuarios WHERE cod_usuario = $1`,
     DevolverUsuario: "Select u.cod_usuario as codUsuario, u.nombres_usuario as nombreUsuario,",
@@ -30,4 +28,5 @@ export const sql_Accesos = {
     getUserById: `SELECT * FROM usuarios WHERE cod_usuario = $1`,
     getByCorreoExcludingId: `SELECT * FROM accesos WHERE correo_acceso = $1 AND cod_usuario != $2`
 };
->>>>>>> 62f9d91 (Cambios realizados)
+
+
